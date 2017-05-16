1251045cad5c95cb35feb9aa178777c5cf9746ec
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-05-16 18:55:54 +0900

    testcode

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -27,18 +27,51 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationTest {
 
+
+    /**
+     *ログインページの画面表示
+     *
+     */
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void loginCheck() {
+        Content html = views.html.loginpage.render(new Form(Application.User.class));
+        assertThat(contentType(html).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("LOGIN");
+        assertThat(contentAsString(html)).contains("PASS");
+        assertThat(contentAsString(html)).contains("ID");
     }
 
+    /**
+     *
+     */
     @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+    public void loginSucces() {
+        FakeApplication app;
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("id", "admin");
+        map.put("pass", "admin");
+
+
+
+
     }
+
+
+        /**
+         *ログインページの画面表示
+         *
+         */
+        @Test
+        public void loginCheck() {
+            Content html = views.html.loginpage.render(new Form(Application.User.class));
+            assertThat(contentType(html).isEqualTo("text/html");
+            assertThat(contentAsString(html)).contains("LOGIN");
+            assertThat(contentAsString(html)).contains("PASS");
+            assertThat(contentAsString(html)).contains("ID");
+        }
+
+
 
 
 }
