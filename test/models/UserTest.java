8dae642952287b61711e178d362de4ed4d4a5876
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-05-17 11:13:07 +0900

    models コメント追加

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -7,8 +7,15 @@ import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
 
 public class UserTest {
+
+    /**
+     *
+     * Model Userのテスト
+     * データベースに対して登録、取得を行い
+     * 登録したものと、取得したものを比較する。
+     */
     @Test
-    public void findByEmail() {
+    public void findByUser() {
         start(fakeApplication(inMemoryDatabase()));
         User user = new User("idAdmin", "nickAdmin", "passAdmin");
         user.save();
