ffc9559735e1f64bdff4bc4231d20854189af0ec
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-05-17 14:57:57 +0900

    models test 修正

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -6,22 +6,40 @@ import org.junit.*;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
 
-public class UserTest {
+public class UserTest extends FakeApp{
 
     /**
      *
      * Model Userのテスト
-     * データベースに対して登録、取得を行い
-     * 登録したものと、取得したものを比較する。
+     * データベースに対して登録を行い、
+     * データベースに正しく値が入っているかを確かめる
      */
     @Test
-    public void findByUser() {
-        start(fakeApplication(inMemoryDatabase()));
+    public void setUserTest() {
         User user = new User("idAdmin", "nickAdmin", "passAdmin");
         user.save();
 
-        User requestuser = User.find.byId("idAdmin");
+        String sql = " SELECT * FROM user WHERE id = :id";
 
-        assertThat(requestuser).isEqualTo(user);
+        List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).setParameter("id", "idAdmin").findList();
+        assertThat(result.get(0).getString("nickname")).isEqualTo("nickAdmin");
+        assertThat(result.get(0).getString("password")).isEqualTo("passAdmin");
+    }
+
+    /**
+     *
+     * Model Userのテスト
+     * データベースから値を取得し、
+     * 登録した値と比較する
+     */
+    @Test
+    public void getUserTest() {
+        User user = new User("idAdmin", "nickAdmin", "passAdmin");
+        user.save();
+
+        User requestUser = User.find.byId("idAdmin");
+
+        assertThat(requestUser.nickname).isEqualTo("nickAdmin");
+        assertThat(requestUser.password).isEqualTo("passAdmin");
     }
 }
