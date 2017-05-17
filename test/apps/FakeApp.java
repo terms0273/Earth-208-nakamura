414fb9db03a67f714513988dbfb601b7d9a95dbc
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-05-17 14:58:44 +0900

    FakeApp.javaの作成

---
+++ test/apps/FakeApp.java
@@ -0,0 +1,52 @@
+package apps;
+
+import com.avaja.ebean.Ebean;
+import net.sf.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+import play.test.FakeApplication;
+import java.io.IOException;
+
+import static play.test.Helpers.*;
+
+public class FakeApp {
+    public static FakeApplication app;
+    public static String createDdl = "";
+    public static String dropDdl = "";
+
+    @BeforeClass
+    public static void startApp() throws IOException {
+        app = fakeApplication(inMemoryDatabase());
+        start(app);
+        String evolutionContent = FileUtils.readFileToString(app.getWrappedApplication()
+                                            .getFile("conf/evolutions/default/1.sql"));
+        String[] splitEvolutionContent = evolutionContent.split("# --- !Ups");
+        String[] upsDowns = splitEvolutionContent[1].split("# --- !Downs");
+        createDdl = upsDowns[0];
+        dropDdl = upsDowns[1];
+    }
+
+    @Before
+    public void createCleanDb() {
+        initDb();
+    }
+
+    public static void initDb() {
+        Ebean.execute(Ebean.createCallableSql(dropDdl));
+        Ebean.execute(Ebean.createCallableSql(createDdl));
+
+        // Ehcacheキャッシュのクリア
+        CacheManager manager = CacheManager.create();
+        Cache cache = manager.getCache("play");
+        cache.removeAll();
+    }
+
+    public static void restartApp() {
+        start(app);
+    }
+
+    @AfterClass
+    public start void stopApp() {
+        stop(app);
+    }
+}
